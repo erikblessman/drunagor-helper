03cca2e278746fcb59231f628e98a31d9bf69c8c
erikblessman <erik.blessman+gh@gmail.com>
2024-01-21 00:11:50 -0700

    Refactored MonsterHealthView to use list of monsters

--- src/views/MonsterHealthView.vue
+++ src/views/MonsterHealthView.vue
@@ -6,10 +6,15 @@
   import MonsterImage from "@/components/MonsterImage.vue";
 
   let monsterDataStore = MonsterDataStore();
-  let monster = ref<MonsterData>(monsterDataStore.find('rotten-flesh'));
+  // let monster = ref<MonsterData>(monsterDataStore.find('rotten-flesh'));
+  let monsters = ref<MonsterData[]>([]);//monsterDataStore.findAllEnabled());
 
   function addMonster(newMonster: MonsterData) {
-    monster.value = newMonster;
+    monsters.value.push(newMonster);
+  }
+
+  function removeMonster(index: number) {
+    monsters.value.splice(index, 1);
   }
 </script>
 
@@ -18,7 +23,13 @@
     <MonsterPicker @pick-monster="addMonster"/>
   </BaseButtonMenu>
   <div class="grid grid-flow-col auto-cols-max" gap-4>
-    <MonsterImage :monster="monster" imgClass="w-48 rounded-full" class="max-w-xs bg-white border-fuchsia-500 border-8 rounded-full shadow dark:bg-gray-800"/>
+        <template v-for="(monster, index) in monsters" :key="index">
+          <MonsterImage
+          :monster="monster"
+          @dblclick="removeMonster(index)"
+          imgClass="w-48 rounded-full"
+          class="max-w-xs bg-white border-fuchsia-500 border-8 rounded-full shadow dark:bg-gray-800"/>
+        </template>
   </div>
 </template>
 
